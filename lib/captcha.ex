bc82f08940eb98c3e1767633224578e7fbaa34ee
Karl Eklund <localpart@gmail.com>
2017-12-09 16:05:43 +0100

    New implementation of day 1 part A with streams

--- lib/captcha.ex
+++ lib/captcha.ex
@@ -1,17 +1,32 @@
 defmodule AdventOfCode2017.Captcha do
   def sum(number) when is_integer(number) do
-    l =
-      number
-      |> Integer.to_charlist
-      |> Enum.map(fn x -> x - ?0 end)
-
-    [i | _] = l
-
-    sum(l, i)
+    number
+    |> digit_list
+    |> sum
   end
 
-  defp sum([], _), do: 0
-  defp sum([h], h), do: h
-  defp sum([h|[h|t]], i), do: h + sum([h|t], i)
-  defp sum([_|t], i), do: sum(t, i)
+  def sum(digit_list) when is_list(digit_list) do
+    length = length(digit_list)
+    numbers =
+      digit_list
+      |> Stream.cycle
+      |> Enum.take(length)
+    nexts =
+      digit_list
+      |> Stream.cycle
+      |> Stream.drop(1)
+      |> Enum.take(length)
+
+    numbers
+    |> Enum.zip(nexts)
+    |> Enum.filter(fn {number, next} -> number == next end)
+    |> Enum.map(fn {number, next} -> number end)
+    |> Enum.sum
+  end
+
+  defp digit_list(number) do
+    number
+    |> Integer.to_charlist
+    |> Enum.map(fn x -> x - ?0 end)
+  end
 end
